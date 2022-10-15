9a5ffb27323f0fea68084d7dac63cab46efff518
DanielDiazRuiz2002 <daniel.diaz@ucsm.edu.pe>
2022-10-14 20:22:58 -0500

    Septima version de la página web

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./assets/Home";
-import Header from "./assets/Header";
-import AboutUs from "./assets/AboutUs";
+import Home from "./Home";
+import Header from "./Header";
+import AboutUs from "./AboutUs";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <h1>Prueba</h1>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}>
